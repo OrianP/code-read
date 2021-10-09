9f1af283d5eda76a061eae235aa6ff915e1e7a06
Orian <77785778+OrianP@users.noreply.github.com>
2021-10-09 20:57:52 +0100

    Add challenge 02

--- 01.js
+++ 01.js
@@ -20,3 +20,21 @@ function transform(arg) {
 // 
 // 
 console.log(transform('hello'));
+// Bonus q: what array method could you use to recreate the same result? 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+function transform1(arg){
+    return arg.split('').reduceRight((acc, val) => acc + val);
+}
+
+// console.log(transform1('hello'))
